87a6345e55b608365cadb78ad0bde443eb8ec115
Ruddy <ruddy.jm@usa.com>
2021-04-18 17:04:45 -0500

    updated Javascript

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let 
+var results = document.getElementById('first');
+console.log(results)
+
+var results = document.getElementsByClassName('fruits');
+console.log(results)
+
+var results = document.getElementsByTagName('h1');
+console.log(results)
+
+//Changing between var and let gives you a different result
