8608bd57ddde6cb9e992412b9663db9b44beca5e
Ruddy <ruddy.jm@usa.com>
2021-04-18 19:32:35 -0500

    added loop and array

--- index.js
+++ index.js
@@ -27,9 +27,16 @@ console.log(results)
 //Chooses one class or id and returns output
 
 var results = document.querySelectorAll('ul.basket > li')  //'ul#id-name > li
-console.log(results)
+console.log(Array.from(results))
 
 //Accesses unordered list from html file
 //Grabs the class named 'basket'
 // '>' Points 'basket' > to 'li' and gives access to the inner list
 //Logs ul class and NodeList
+
+for (var i = 0; i< results.length; i++) {
+     results[i].textContent = ' *REDACTED* '
+}
+
+//Loop modifies the behavior of (Array.from(results))
+//and .textContent modifies the text of the array 
