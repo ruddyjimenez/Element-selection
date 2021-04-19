31b113a9523c3c0d869df5d50db0a45174fc5f00
Ruddy <ruddy.jm@usa.com>
2021-04-18 19:18:18 -0500

    uncomment js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// var results = document.getElementById('first');
-// console.log(results)
+var results = document.getElementById('first');
+console.log(results)
 
-// var results = document.getElementsByClassName('fruits');
-// console.log(results)
+var results = document.getElementsByClassName('fruits');
+console.log(results)
 
-// var results = document.getElementsByTagName('h1');
-// console.log(results)
+var results = document.getElementsByTagName('h1');
+console.log(results)
 
 //Changing between var and let gives you a different result
 
